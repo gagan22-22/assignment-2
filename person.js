a0833022155810dc86bd303262252d52521e3b1a
gagan22-22 <>
2024-01-28 15:00:47 +0530

    first commit

---
+++ person.js
@@ -0,0 +1,7 @@
+function persondetail(persondet) {
+    for(i of persondet){
+        console.log(i)
+    }
+}
+let person=[{Name:"gagan",age:20,occupation:"engineer"},{Name:"ajay",age:20,occupation:"engineer"},{Name:"gautam",age:20,occupation:"engineer"}]
+persondetail(person)
